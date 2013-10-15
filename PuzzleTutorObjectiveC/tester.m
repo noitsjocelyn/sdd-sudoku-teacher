a09ed66107c7e542da58c52b7617e8603d67665b
jonathanboom <jonathanlynchboom@gmail.com>
2013-10-15 17:18:25 -0400

    removed extraneous old code and cleaned up names

--- PuzzleTutorObjectiveC/tester.m
+++ PuzzleTutorObjectiveC/tester.m
@@ -11,28 +11,25 @@ int main ()
     
     Puzzle *aPuzzle = [[Puzzle alloc] init];
     NSLog(@"Puzzle allocation successful.");
+	
+    char *inputString = "000105000140000670080002400063070010900000003010090520007200080026000035000409000"; 
     short *inputPuzzle = calloc(81, sizeof(short));
-    short *inputPuzzle2 = calloc(81, sizeof(short));
-    char *inputString = calloc(81, sizeof(char));
-    char *inputString2 = calloc(81, sizeof(char));
-    inputString2 = "000105000140000670080002400063070010900000003010090520007200080026000035000409000"; 
-    inputString = "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-    for (int i = 0; i < 81; i++)
+    for (int i = 0; i < 81; ++i)
     {
-    	char c = inputString[i];
-        char d = inputString2[i];
-    	inputPuzzle[i] = (short)(c - '0');
-        inputPuzzle2[i] = (short)(d - '0');
+        char c = inputString[i];
+        inputPuzzle[i] = (short)(c - '0');
     }
+	
     NSLog(@"Attempting initialization.");
-    for (int i = 0; i < 81; i++)
+    for (int i = 0; i < 81; ++i)
     {
-        if (inputPuzzle2[i] != 0)
+        if (inputPuzzle[i] != 0)
         {
-            [aPuzzle putInValue:(i * 9 + inputPuzzle2[i])];
+            [aPuzzle putInValue:(i * 9 + inputPuzzle[i])];
         }
     }
     NSLog(@"Initialization successful.");
+	
     short *results = [aPuzzle findSquareWithOneAvailableValue];
     if (results[0] == 1 && results[1] == 1 && results[2] == 7 && results[3] == 9)
     {
