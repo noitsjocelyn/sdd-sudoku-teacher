7bc673937faa31b054b4146ce83bd6e71a1e82c2
arnala <allie.arnal@gmail.com>
2013-10-24 17:43:55 -0400

    Initial Player/Game classes. Unsure if seperate Player class is necessary.

---
+++ Game/Player.h
@@ -0,0 +1,16 @@
+/* The Player Class.
+ * Methods:
+ *    See Player.m
+ */
+
+#import <Foundation/Foundation.h>
+
+@class Game;
+
+@interface Player : NSObject 
+{
+}
+
+- (void) getPlayerInput: (Game*) Board;
+
+@end
