746f455f3f442d010f573d6f223ca3563328e0dd
jonathanboom <jonathanlynchboom@gmail.com>
2013-10-25 20:39:25 -0400

    added basic sudoku board drawing

---
+++ Sudoku Teacher/Sudoku Teacher/PPSudokuView.h
@@ -0,0 +1,13 @@
+//
+//  PPSudokuView.h
+//  Sudoku Teacher
+//
+//  Created by Jonathan on 10/25/13.
+//  Copyright (c) 2013 Puzzle Professors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPSudokuView : UIView
+
+@end
---
+++ Sudoku Teacher/Sudoku Teacher/PPSudokuView.m
@@ -0,0 +1,49 @@
+//
+//  PPSudokuView.m
+//  Sudoku Teacher
+//
+//  Created by Jonathan on 10/25/13.
+//  Copyright (c) 2013 Puzzle Professors. All rights reserved.
+//
+
+#import "PPSudokuView.h"
+
+@implementation PPSudokuView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+	// Get the context and set the color
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSetFillColorWithColor(context, [UIColor blackColor].CGColor);
+	// Draw the horizontal lines
+	CGContextFillRect(context, CGRectMake(2.0, 37.0, 316.0, 0.5));
+	CGContextFillRect(context, CGRectMake(2.0, 72.0, 316.0, 0.5));
+	CGContextFillRect(context, CGRectMake(2.0, 107.0, 316.0, 1.0));
+	CGContextFillRect(context, CGRectMake(2.0, 142.0, 316.0, 0.5));
+	CGContextFillRect(context, CGRectMake(2.0, 178.5, 316.0, 0.5));
+	CGContextFillRect(context, CGRectMake(2.0, 213.5, 316.0, 1.0));
+	CGContextFillRect(context, CGRectMake(2.0, 248.5, 316.0, 0.5));
+	CGContextFillRect(context, CGRectMake(2.0, 283.5, 316.0, 0.5));
+	// Draw the vertical lines
+	CGContextFillRect(context, CGRectMake(37.0, 2.0, 0.5, 316.0));
+	CGContextFillRect(context, CGRectMake(72.0, 2.0, 0.5, 316.0));
+	CGContextFillRect(context, CGRectMake(107.0, 2.0, 1.0, 316.0));
+	CGContextFillRect(context, CGRectMake(142.0, 2.0, 0.5, 316.0));
+	CGContextFillRect(context, CGRectMake(178.5, 2.0, 0.5, 316.0));
+	CGContextFillRect(context, CGRectMake(213.5, 2.0, 1.0, 316.0));
+	CGContextFillRect(context, CGRectMake(248.5, 2.0, 0.5, 316.0));
+	CGContextFillRect(context, CGRectMake(283.5, 2.0, 0.5, 316.0));
+}
+
+@end
