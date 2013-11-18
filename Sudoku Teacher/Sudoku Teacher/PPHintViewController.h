88a3a004c97b9d03d142854af11fcb3246ffedbf
jonathanboom <jonathanlynchboom@gmail.com>
2013-11-18 16:35:05 -0500

    Classes for hints

---
+++ Sudoku Teacher/Sudoku Teacher/PPHintViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPHintViewController.h
+//  Sudoku Teacher
+//
+//  Created by Jonathan on 11/18/13.
+//  Copyright (c) 2013 Puzzle Professors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPHintViewController : UIViewController
+
+@end
