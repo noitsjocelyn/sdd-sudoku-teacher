7616c028d6dd8503d0cdc2b8f3571a2364bf4fde
jonathanboom <jonathanlynchboom@gmail.com>
2013-11-19 11:23:46 -0500

    Added hint two, added animated switching between first two hints

--- Sudoku Teacher/Sudoku Teacher/PPHintViewController.h
+++ Sudoku Teacher/Sudoku Teacher/PPHintViewController.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 #import "PPSudokuGameViewController.h"
 
+#define ANIMATE_TIME 0.25
+
 @class HintsMaker;
 @class Puzzle;
 
@@ -16,11 +18,22 @@
 {
     HintsMaker *aHintMaker;
     NSArray *hints;
+    CGRect hintTwoShownFrame;
+    CGRect hintThreeShownFrame;
+    CGRect hintTwoHiddenFrame;
+    CGRect hintThreeHiddenFrame;
 }
 
 @property (strong) Puzzle *puzzleData;
 @property (weak, nonatomic) id<PPSudokuGameProtocol> delegate;
+@property (weak, nonatomic) IBOutlet UIView *hintOneView;
+@property (weak, nonatomic) IBOutlet UIView *hintTwoView;
 @property (weak, nonatomic) IBOutlet UILabel *hintOneLabel;
-@property (weak, nonatomic) IBOutlet UIButton *hintOneButton;
+@property (weak, nonatomic) IBOutlet UILabel *hintTwoLabel;
+
+- (IBAction)showHintOne:(id)sender;
+- (IBAction)showHintTwo:(id)sender;
+- (IBAction)showHintThree:(id)sender;
+
 
 @end
--- Sudoku Teacher/Sudoku Teacher/PPHintViewController.m
+++ Sudoku Teacher/Sudoku Teacher/PPHintViewController.m
@@ -32,6 +32,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     aHintMaker = [[HintsMaker alloc] init];
+    [self setUpHintPositions];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -39,6 +40,8 @@
     [super viewWillAppear:animated];
     hints = [aHintMaker createHints:self.puzzleData];
     [self.hintOneLabel setText:[[hints objectAtIndex:0] hintText]];
+    [self.hintTwoLabel setText:[[hints objectAtIndex:1] hintText]];
+//    [self.hintThreeLabel setText:[[hints objectAtIndex:2] hintText]];
     NSLog(@"%@", hints);
 }
 
@@ -51,6 +54,43 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setUpHintPositions
+{
+    CGFloat viewW = 320.0;
+    CGFloat viewH = 320.0;
+    CGFloat titleH = 64.0;
+    CGFloat buttonH = 48.0;
+    // Title bar has 0 height under iOS 6.1
+    if ([[[UIDevice currentDevice] systemVersion] intValue] == 6)
+    {
+        titleH = 0.0;
+    }
+    hintTwoShownFrame = CGRectMake(0.0, titleH + buttonH, viewW, viewH);
+    hintThreeShownFrame = CGRectMake(0.0, titleH + (2 * buttonH), viewW, viewH);
+    hintTwoHiddenFrame = CGRectMake(0.0, titleH + viewH, viewW, viewH);
+    hintThreeShownFrame = CGRectMake(0.0, titleH + viewH + buttonH, viewW, viewH);
+}
+
+- (IBAction)showHintOne:(id)sender {
+    [UIView animateWithDuration:ANIMATE_TIME animations:^(void) {
+        [self.hintTwoView setFrame:hintTwoHiddenFrame];
+//        [self.hintThreeView setFrame:hintThreeHiddenFrame];
+    }];
+}
+
+- (IBAction)showHintTwo:(id)sender {
+    [UIView animateWithDuration:ANIMATE_TIME animations:^(void) {
+        [self.hintTwoView setFrame:hintTwoShownFrame];
+    } completion:^(BOOL finished) {
+//        [self.hintThreeView setFrame:hintThreeHiddenFrame];
+    }];
+}
+
+- (IBAction)showHintThree:(id)sender {
+    [self.hintTwoView setFrame:hintTwoHiddenFrame];
+//    [self.hintThreeView setFrame:hintThreeShownFrame];
 }
 
 @end
